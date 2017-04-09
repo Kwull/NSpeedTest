2388187d5f35386959de99c2ee14146b12bd192d
Mike Cousins <mike@mikecousins.com>
2017-04-09 14:48:56 -0600

    Adding .Net Standard projects

---
+++ NSpeedTest.Standard/SpeedTestWebClient.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NSpeedTest
+{
+    internal class SpeedTestWebClient : HttpClient
+    {
+        public int ConnectionLimit { get; set; }
+
+        public SpeedTestWebClient()
+        {
+            ConnectionLimit = 10;
+        }
+
+        public async Task<T> GetConfig<T>(string url)
+        {
+            var data = await GetStringAsync(url);
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using (var reader = new StringReader(data))
+            {
+                return (T)xmlSerializer.Deserialize(reader);
+            }
+        }
+
+        private static Uri AddTimeStamp(Uri address)
+        {
+            var uriBuilder = new UriBuilder(address);
+            var query = QueryHelpers.ParseQuery(uriBuilder.Query);
+            query["x"] = DateTime.Now.ToFileTime().ToString(CultureInfo.InvariantCulture);
+            uriBuilder.Query = query.ToString();
+            return uriBuilder.Uri;
+        }
+    }
+}
